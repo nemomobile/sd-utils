f8a0601ea765dbbcd71704ba5cf166d0b2d2eb14
Jarko Vihriala <jarko.vihriala@jollamobile.com>
2013-10-30 15:01:51 +0200

    [review] changes after review.

--- scripts/tracker-sd-indexing.sh
+++ scripts/tracker-sd-indexing.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "Triggered" >> /tmp/logi
+
 if [ -b /dev/sdcard  ]
 then
-	gsettings set org.freedesktop.Tracker.Miner.Files index-removable-devices true
-	gsettings set org.freedesktop.Tracker.Miner.Files index-recursive-directories "['&DESKTOP', '&DOCUMENTS', '&DOWNLOAD', '&MUSIC', '&PICTURES', '&VIDEOS', '/storage/extSdCard']"
-else
-	gsettings set org.freedesktop.Tracker.Miner.Files index-removable-devices false	
+	if [ "$(gsettings get org.freedesktop.Tracker.Miner.Files index-removable-devices)" = "false" ]
+	then
+		gsettings set org.freedesktop.Tracker.Miner.Files index-removable-devices true
+	fi
+
+	if [ "$(gsettings get org.freedesktop.Tracker.Miner.Files index-recursive-directories | grep extSdCard)" = "" ]
+	then
+		gsettings set org.freedesktop.Tracker.Miner.Files index-recursive-directories "['&DESKTOP', '&DOCUMENTS', '&DOWNLOAD', '&MUSIC', '&PICTURES', '&VIDEOS', '/storage/extSdCard']"
+	fi
 fi
 
 systemctl --user restart tracker-miner-fs.service
