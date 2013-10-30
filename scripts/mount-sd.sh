e6c95293d917b583418da6419c4f199645360f3e
Jarko Vihriala <jarko.vihriala@jollamobile.com>
2013-10-30 23:24:21 +0200

    [rights] rework the rights issue.

--- scripts/mount-sd.sh
+++ scripts/mount-sd.sh
@@ -4,7 +4,7 @@ SDCARD=/dev/sdcard
 MNT=/mnt/extSdCard
 
 if [ "$ACTION" = "add" ]; then
-	chmod 755 /storage
+	#chmod 755 /storage
 	if [ -b /dev/mmcblk1p1 ]; then
 		ln -sf /dev/mmcblk1p1 $SDCARD
 	elif [ -b /dev/mmcblk1 ]; then
