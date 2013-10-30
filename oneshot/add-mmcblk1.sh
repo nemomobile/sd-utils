1f023dd410345e08c05efe4e84b44a91865ef654
Jarko Vihriala <jarko.vihriala@jollamobile.com>
2013-10-30 12:43:19 +0200

    [path] mount point fixed.

--- oneshot/add-mmcblk1.sh
+++ oneshot/add-mmcblk1.sh
@@ -2,5 +2,5 @@
 
 if [ "$(grep extSdCard /etc/fstab)" == "" ]
 then
-	echo "/dev/mmcblk1p1 /storage/extSdCard auto rw,noauto,nosuid,nodev,users,umask=0000,sync 0 0" >> /etc/fstab
+	echo "/dev/sdcard /storage/extSdCard auto rw,noauto,nosuid,nodev,users,umask=0000,sync 0 0" >> /etc/fstab
 fi
