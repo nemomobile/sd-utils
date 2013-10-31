ec96c903e96cfc406e2ed029b11e0f57cd0df23e
Jarko Vihriala <jarko.vihriala@jollamobile.com>
2013-10-31 22:47:22 +0200

    [fstab] update fstab to match current mount points.

--- oneshot/add-mmcblk1.sh
+++ oneshot/add-mmcblk1.sh
@@ -2,5 +2,5 @@
 
 if [ "$(grep extSdCard /etc/fstab)" == "" ]
 then
-	echo "/dev/sdcard /storage/extSdCard auto rw,noauto,nosuid,nodev,users,umask=0000,sync 0 0" >> /etc/fstab
+	echo "/dev/sdcard /run/user/100000/media/sdcard auto rw,noauto,nosuid,nodev,users,umask=0000,sync 0 0" >> /etc/fstab
 fi
