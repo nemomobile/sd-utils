f4c30e4a21b72dfb20bbd0c97f34edb3ed091789
Mohammed Hassan <mohammed.hassan@jolla.com>
2014-03-05 17:00:31 +0200

    [sd-utils] adjust mount options for various filesystems

--- scripts/mount-sd.sh
+++ scripts/mount-sd.sh
@@ -7,7 +7,7 @@ DEF_UID=$(grep "^UID_MIN" /etc/login.defs |  tr -s " " | cut -d " " -f2)
 DEF_GID=$(grep "^GID_MIN" /etc/login.defs |  tr -s " " | cut -d " " -f2)
 DEVICEUSER=$(getent passwd $DEF_UID | sed 's/:.*//')
 MNT=/media/sdcard
-MOUNT_OPTS="dirsync,flush,noatime,discard,users"
+MOUNT_OPTS="dirsync,noatime,users"
 if [ -z "${ACTION}" ] || [ -z "${DEVNAME}" ] || [ -z "${ID_FS_UUID}" ] || [ -z "${ID_FS_TYPE}" ]; then
 	exit 1
 fi
@@ -17,7 +17,10 @@ if [ "$ACTION" = "add" ]; then
     chown $DEF_UID:$DEF_GID $MNT $MNT/${ID_FS_UUID}
 
     case "${ID_FS_TYPE}" in
-	vfat|ntfs|exfat)
+	vfat|exfat)
+	    mount ${DEVNAME} $MNT/${ID_FS_UUID} -o uid=$DEF_UID,gid=$DEF_GID,$MOUNT_OPTS,utf8,flush,discard || /bin/rmdir $MNT/${ID_FS_UUID}
+	    ;;
+	ntfs)
 	    mount ${DEVNAME} $MNT/${ID_FS_UUID} -o uid=$DEF_UID,gid=$DEF_GID,$MOUNT_OPTS,utf8 || /bin/rmdir $MNT/${ID_FS_UUID}
 	    ;;
 	*)
