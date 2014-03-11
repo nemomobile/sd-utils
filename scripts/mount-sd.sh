383618bd2c5e3352b1e147640a817221ab43f3f5
Islam Amer <islam.amer@jollamobile.com>
2014-03-11 02:32:55 +0200

    [sdcard] rework sdcard handling again to workaround various tracker limitations

--- scripts/mount-sd.sh
+++ scripts/mount-sd.sh
@@ -8,27 +8,60 @@ DEF_GID=$(grep "^GID_MIN" /etc/login.defs |  tr -s " " | cut -d " " -f2)
 DEVICEUSER=$(getent passwd $DEF_UID | sed 's/:.*//')
 MNT=/media/sdcard
 MOUNT_OPTS="dirsync,noatime,users"
-if [ -z "${ACTION}" ] || [ -z "${DEVNAME}" ] || [ -z "${ID_FS_UUID}" ] || [ -z "${ID_FS_TYPE}" ]; then
-	exit 1
+ACTION=$1
+DEVNAME=$2
+
+if [ -z "${ACTION}" ] || [ -z "${DEVNAME}" ]; then
+    exit 1
 fi
 
-if [ "$ACTION" = "add" ]; then
-    mkdir -p $MNT/${ID_FS_UUID}
-    chown $DEF_UID:$DEF_GID $MNT $MNT/${ID_FS_UUID}
+systemd-cat -t mount-sd /bin/echo "Called to ${ACTION} ${DEVNAME}"
 
-    case "${ID_FS_TYPE}" in
+if [ "$ACTION" = "add" ]; then
+    eval "$(/sbin/blkid -o export /dev/$2)"
+
+    if [ -z "${UUID}" ] || [ -z "${TYPE}" ]; then
+        exit 1
+    fi
+
+    # This hack is here to delay mounting the sdcard until tracker is ready
+    export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$DEF_UID/dbus/user_bus_socket
+    count=1
+    while true; do 
+        test $count -ge 64 && break
+        MINER_STATUS="$(dbus-send --type=method_call --print-reply --session --dest=org.freedesktop.Tracker1.Miner.Files /org/freedesktop/Tracker1/Miner/Files org.freedesktop.Tracker1.Miner.GetStatus | grep -o 'Idle')"
+        STORE_STATUS="$(dbus-send --type=method_call --print-reply --session --dest=org.freedesktop.Tracker1 /org/freedesktop/Tracker1/Status org.freedesktop.Tracker1.Status.GetStatus | grep -o 'Idle')"
+        test "$MINER_STATUS" = "Idle" -a "$STORE_STATUS" = "Idle" && break
+        systemd-cat -t mount-sd /bin/echo "Waiting $count seconds for tracker"
+        sleep $count ; 
+        count=$(( count + count ))
+    done
+
+    test -d $MNT/${UUID} || mkdir -p $MNT/${UUID}
+    chown $DEF_UID:$DEF_GID $MNT $MNT/${UUID}
+    touch $MNT/${UUID}
+
+    case "${TYPE}" in
 	vfat|exfat)
-	    mount ${DEVNAME} $MNT/${ID_FS_UUID} -o uid=$DEF_UID,gid=$DEF_GID,$MOUNT_OPTS,utf8,flush,discard || /bin/rmdir $MNT/${ID_FS_UUID}
+	    mount ${DEVNAME} $MNT/${UUID} -o uid=$DEF_UID,gid=$DEF_GID,$MOUNT_OPTS,utf8,flush,discard || /bin/rmdir $MNT/${UUID}
 	    ;;
 	# NTFS support has not been tested but it's being left to please the ego of an engineer!
 	ntfs)
-	    mount ${DEVNAME} $MNT/${ID_FS_UUID} -o uid=$DEF_UID,gid=$DEF_GID,$MOUNT_OPTS,utf8 || /bin/rmdir $MNT/${ID_FS_UUID}
+	    mount ${DEVNAME} $MNT/${UUID} -o uid=$DEF_UID,gid=$DEF_GID,$MOUNT_OPTS,utf8 || /bin/rmdir $MNT/${UUID}
 	    ;;
 	*)
-	    mount ${DEVNAME} $MNT/${ID_FS_UUID} -o $MOUNT_OPTS || /bin/rmdir $MNT/${ID_FS_UUID}
+	    mount ${DEVNAME} $MNT/${UUID} -o $MOUNT_OPTS || /bin/rmdir $MNT/${UUID}
 	    ;;
     esac
+    test -d $MNT/${UUID} && touch $MNT/${UUID}
+    test -d $MNT/${UUID} && systemctl-user restart tracker-miner-fs.service
+    systemd-cat -t mount-sd /bin/echo "Finished ${ACTION}ing ${DEVNAME} of type ${TYPE} at $MNT/${UUID}"
+
 else
     DIR=$(mount | grep -w ${DEVNAME} | cut -d \  -f 3)
-    umount $DIR || umount -l $DIR
+    if [ -n "${DIR}" ] ; then
+        umount $DIR || umount -l $DIR
+        systemd-cat -t mount-sd /bin/echo "Finished ${ACTION}ing ${DEVNAME} at ${DIR}"
+    fi
 fi
+
