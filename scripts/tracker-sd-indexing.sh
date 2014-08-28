e759678484b102e57b08b3b2db5bbc18ba2dae0b
Islam Amer <islam.amer@jollamobile.com>
2014-08-28 15:58:32 +0300

    [schema] change default fs miner settings to better handle android storage

--- scripts/tracker-sd-indexing.sh
+++ scripts/tracker-sd-indexing.sh
@@ -7,6 +7,6 @@ gsettings set org.freedesktop.Tracker.Miner.Files crawling-interval 0
 gsettings set org.freedesktop.Tracker.Miner.Files enable-writeback false
 gsettings set org.freedesktop.Tracker.Miner.Files removable-days-threshold 30
 gsettings set org.freedesktop.Tracker.Miner.Files index-single-directories "['$HOME']"
-gsettings set org.freedesktop.Tracker.Miner.Files index-recursive-directories "['&DESKTOP', '&DOCUMENTS', '&DOWNLOAD', '&MUSIC', '&PICTURES', '&VIDEOS', '$HOME/android_storage']"
+gsettings set org.freedesktop.Tracker.Miner.Files index-recursive-directories "['&DESKTOP', '&DOCUMENTS', '&DOWNLOAD', '&MUSIC', '&PICTURES', '&VIDEOS', '$HOME/android_storage/DCIM', '$HOME/android_storage/Download', '$HOME/android_storage/Pictures', '$HOME/android_storage/Podcasts', '$HOME/android_storage/Music']"
 gsettings set org.freedesktop.Tracker.Miner.Files ignored-directories-with-content "[ 'backup.metadata', '.nomedia' ]"
 gsettings set org.freedesktop.Tracker.Miner.Files ignored-directories "[ 'po', 'CVS', 'core-dumps', 'lost+found', '$HOME/android_storage/Android' ]"
