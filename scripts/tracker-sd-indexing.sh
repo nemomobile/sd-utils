98620e9c24f24715aafdbb81fb8ff34b64e4ef7f
Islam Amer <islam.amer@jollamobile.com>
2014-11-12 18:30:53 +0200

    [tracker] change miner initial sleep to 30 seconds to not hammer the system during bootup

--- scripts/tracker-sd-indexing.sh
+++ scripts/tracker-sd-indexing.sh
@@ -4,6 +4,7 @@ set -e
 
 gsettings set org.freedesktop.Tracker.Miner.Files index-removable-devices true
 gsettings set org.freedesktop.Tracker.Miner.Files crawling-interval 0
+gsettings set org.freedesktop.Tracker.Miner.Files initial-sleep 30
 gsettings set org.freedesktop.Tracker.Miner.Files enable-writeback false
 gsettings set org.freedesktop.Tracker.Miner.Files removable-days-threshold 30
 gsettings set org.freedesktop.Tracker.Miner.Files index-single-directories "['$HOME']"
